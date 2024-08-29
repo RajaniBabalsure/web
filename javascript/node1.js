a3f96d66872b97bd80f7c1cd2b536ea4af7ff0d6
rajani babalsure <rajanibabalsure61@gmail.com>
2024-08-29 19:34:40 +0530

    practies.1.js

---
+++ javascript/node1.js
@@ -0,0 +1,10 @@
+let arr = [90,74,86,53,29,20,19,72,91,68,94,1,19,27,88,];
+
+let sum = 0;
+
+for (let num of arr ) {
+    sum += num;
+
+}
+
+console.log ("sum  of all elements in the array:", sum);
